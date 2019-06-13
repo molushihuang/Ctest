ea2d44fb2c3a8e3da2346733161cc0559e0f7d0a
陌路拾荒 <xieqiudongv5@163.com>
2019-06-13 17:58:45 +0800

    动态分配内存，扩展内存，字符处理代码

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <time.h>
 #include "md5.h"
+#include<Windows.h>
 using namespace std;
 
 int g = 10;
@@ -20,6 +21,37 @@ int getNumber(int a, int b) {
 
 int func() {
 	return g;
+}
+
+void msg(char* msg, char* title) {
+	//C语言里面字符串用char指针代替
+	MessageBox(0, msg, title, 0);
+
+}
+
+
+//转换为小写
+void mystrlwr(char str[], int len) {
+	int i = 0;
+	for (; i < len; i++) {
+		//A-Z 字母 a-Z
+		if (str[i] >= 'A' && str[i] <= 'Z') {
+			str[i] = str[i] - 'A' + 'a';
+		}
+	}
+
+}
+
+//练习：删除字符串中指定的字符
+void delchar(char* str, char del) {
+	char* p = str;
+	while (*str != '\0') {
+		if (*str != del) {
+			*p++ = *str;
+		}
+		str++;
+	}
+	*p = '\0';
 }
 
 int main() {
@@ -77,15 +109,85 @@ int main() {
 	//cout << result << endl;
 
 	//字符串处理部分
-	string str = "123456789";
+	//string str = "123456789";
 	//cout << str.replace(2, 3, "ffdd")<<endl;
-	cout << str.size() << "+"<<str.length()<<endl;
+	//cout << str.size() << "+"<<str.length()<<endl;
 	//cout << str + "sdada" <<endl;
 	//cout << str.append("dsfsf") <<endl;
 	//cout << str.find_first_of("2") << endl;
 	//cout << str.find_first_of("9") << endl;
 	//cout << str.substr(str.size() - 2, str.size()) << endl;
 	//cout << str.substr(2, 7) << endl;
+
+	//printf("%#x\n", msg);
+	//printf("%#x\n", &msg);
+	////函数指针
+	//void(*fun_p)(char* msg, char* title) = msg;
+	//fun_p("消息内容", "标题");
+
+	//动态给数组分配内存，并释放
+	//int len;
+	//printf("输入数组的长度：");
+	//scanf("%d", &len);
+	////开辟内存，大小内存len * 4 字节
+	//int* p = (int*)malloc(len * sizeof(int));//p:数组的首地址
+	//for (int i = 0; i < len; i++) {
+	//	p[i] = rand() % 100;
+	//	printf("%d,%#x\n", p[i], &p[i]);
+	//}
+	////手动释放内存
+	////free(p);
+
+	////重新分配内存部分
+	//int addLen;
+	//printf("输入需要增加的数组长度：");
+	//scanf("%d",&addLen);
+	//int* p2 = (int*)realloc(p,sizeof(int)*(len+addLen));
+	//if (p2 == NULL) {
+	//	printf("重新分配内存失败");
+	//}
+	//for (int i = 0; i < (len + addLen); i++) {
+	//	p2[i] = rand() % 100;
+	//	printf("%d,%#x\n", p2[i], &p2[i]);
+	//}
+
+	////手动释放内存 p2释放内存 p也会释放，因为给p2分配内存的时候要么p已经释放，要么p2、p指向统一地址区域
+	//if (p2 != NULL) {
+	//	free(p2);
+	//	p2 = NULL;
+	//}
+
+    //strset 把字符串s中的所有字符都设置成字符c
+	//char str[] = "internet change the world!";
+	//_strset(str, 'w');
+	//printf("%s\n", str);
+
+	//strrev 把字符串s的所有字符的顺序颠倒过来
+	/*char str[] = "internet change the world!";
+	_strrev(str);
+	printf("%s\n", str);*/
+
+	//atoi 字符串转为int类型
+	//atol()：将字符串转换为长整型值
+	/*char str[] = "a78";
+	int r = atoi(str);    
+	printf("%d\n", r);*/
+
+	//strupr转换为大写
+	//char str[] = "CHINA motherland!";
+	//_strupr(str);
+	//printf("%s\n", str);
+
+	//转换为小写
+	//char str[] = "CHINA motherland!";
+	//mystrlwr(str, strlen(str));
+	//printf("%s\n", str);
+
+	//删除字符
+	char str[] = "vencent ppqq";
+	delchar(str, 't');
+	printf("%s\n", str);
+	
 
 	return getNumber(10, 36);
 }
